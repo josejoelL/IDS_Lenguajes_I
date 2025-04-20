a14c8638d978a97ee2b63d9a07d37f2044d1304c
JoJo <18170365@itculiacan.edu.mx>
2025-04-20 02:09:58 +0000

    agregar un nuevo archivo

--- A1.cpp
+++ A1.cpp
@@ -1,17 +1,13 @@
 #include <iostream>  
-
 int main()
 {
     int edad = 0;
-
     std::cout << "Ingrese edad: ";
     std::cin >> edad; 
-
     if (edad > 17) {
         printf("En efecto es mayor de edad");
     } else {
         printf("Lo sentimos pero no es mayor de edad"); 
     }
-
     return 0;
 }
